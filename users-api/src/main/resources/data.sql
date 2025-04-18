cc9b52c3cae67e184a0b1c39aa3be3bc7a0646ff
Manuel Herrera <98495072+Herre1@users.noreply.github.com>
2025-04-18 00:01:13 -0500

    New Front

--- users-api/src/main/resources/data.sql
+++ users-api/src/main/resources/data.sql
@@ -1,3 +1,12 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    role INTEGER
+);
+
+
 INSERT INTO users (username, firstname, lastname, role) VALUES
   ('admin', 'Foo', 'Bar', 1),
   ('johnd', 'John', 'Doe', 0),
